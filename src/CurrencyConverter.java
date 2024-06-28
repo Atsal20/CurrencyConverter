5e5c97722acb98cd71cf8fcdfb1000b6a70c1d05
Garces Alan <alan18gp@gmail.com>
2024-06-27 22:17:23 -0600

    nueva implementacion

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -3,93 +3,49 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Properties;
 import java.util.Scanner;
 
 public class CurrencyConverter {
-    private static final String API_KEY ="b85f6567c00dcdfcc6e3ab32";
-    private static final String BASE_URL ="https://v6.exchangerate-api.com/v6/";
+
+    private static final String BASE_URL = "https://v6.exchangerate-api.com/v6/";
+    private static final String COUNTRIES_API_URL = "https://restcountries.com/v3.1";
+    private static final String CONFIG_FILE_PATH = "config.properties";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("¡Bienvenido al Conversor de Moneda!");
-        System.out.println("Este programa le permitirá convertir monedas entre diferentes países.");
 
         boolean continuar = true;
         while (continuar) {
-            System.out.println("Seleccione una opción:");
-            System.out.println("1. EUR a USD");
-            System.out.println("2. EUR a MXN");
-            System.out.println("3. USD a EUR");
-            System.out.println("4. USD a MXN");
-            System.out.println("5. MXN a EUR");
-            System.out.println("6. MXN a USD");
-            System.out.println("7. Otro");
-            System.out.println("0. Salir");
-            System.out.print("Opción: ");
+            printOptions();
             int opcion = scanner.nextInt();
-            String fromCurrency = "";
-            String toCurrency = "";
-            double amount;
-            switch (opcion) {
-                case 1:
-                    fromCurrency = "EUR";
-                    toCurrency = "USD";
-                    break;
-                case 2:
-                    fromCurrency = "EUR";
-                    toCurrency = "MXN";
-                    break;
-                case 3:
-                    fromCurrency = "USD";
-                    toCurrency = "EUR";
-                    break;
-                case 4:
-                    fromCurrency = "USD";
-                    toCurrency = "MXN";
-                    break;
-                case 5:
-                    fromCurrency = "MXN";
-                    toCurrency = "EUR";
-                    break;
-                case 6:
-                    fromCurrency = "MXN";
-                    toCurrency = "USD";
-                    break;
-                case 7:
-                    System.out.println("Ingrese el nombre del primer país:");
-                    String fromCountry = scanner.next();
-                    System.out.println("Ingrese el nombre del segundo país:");
-                    String toCountry = scanner.next();
-                    try {
-                        fromCurrency = getCurrencyCodeFromAPI(fromCountry);
-                        toCurrency = getCurrencyCodeFromAPI(toCountry);
-                    } catch (Exception e) {
-                        System.out.println("Error al obtener la moneda: " + e.getMessage());
-                        continue;
-                    }
-                    break;
-                case 0:
-                    System.out.println("Gracias por usar nuestra API\n");
-                    continuar = false;
-                    break;
-                default:
-                    System.out.println("Opción no válida.");
-                    continue;
-
+            if (!isValidInput(opcion)) {
+                System.out.println("Entrada inválida. Por favor, seleccione una opción válida.");
+                continue;
             }
-            System.out.println("Ingrese la cantidad a convertir:");
-            amount = scanner.nextDouble();
+            ConversionOption option = ConversionOption.values()[opcion];
 
+            if (option == ConversionOption.EXIT) {
+                System.out.println("Gracias por usar nuestra API\n");
+                continuar = false;
+                break;
+            }
             try {
+                String fromCurrency = getCurrencyCodeFromAPI(option.getFromCurrency());
+                String toCurrency = getCurrencyCodeFromAPI(option.getToCurrency());
+                double amount = Double.parseDouble(scanner.nextLine());
                 double convertedAmount = convertCurrency(fromCurrency, toCurrency, amount);
-                String formattedResult = String.format("%.2f", convertedAmount);
-                System.out.println(amount + " " + fromCurrency + " = " + formattedResult + " " + toCurrency);
+                System.out.printf("%.2f %s = %.2f %s%n", amount, fromCurrency, convertedAmount, toCurrency);
             } catch (Exception e) {
-                System.out.println("Error al realizar la conversión: " + e.getMessage());
+                System.out.println("Error: " + e.getMessage());
             }
 
             System.out.println("\n¿Desea realizar otra conversión? (S/N):");
@@ -98,57 +54,129 @@ public class CurrencyConverter {
         }
         System.out.println("Programa finalizado. Gracias por usar la API!");
         scanner.close();
-
     }
+
+    private static void printOptions() {
+        System.out.println("Seleccione una opción:");
+        for (ConversionOption option : ConversionOption.values()) {
+            System.out.println(option.ordinal() + ". " + option.name());
+        }
+        System.out.println("0. Salir");
+        System.out.print("Opción: ");
+    }
+
+    private static boolean isValidInput(int opcion) {
+        return opcion >= 0 && opcion < ConversionOption.values().length;
+    }
+
     public static double convertCurrency(String fromCurrency, String toCurrency, double amount) throws Exception {
-        String url = BASE_URL + API_KEY + "/latest/" + fromCurrency;
+        String url = BASE_URL + readApiKey() + "/latest/" + fromCurrency;
+        HttpClient client = HttpClient.newHttpClient(); // Mueve esta línea fuera del try-with-resources
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .build();
+        try (HttpRequest request = HttpRequest.newBuilder()
+               .uri(URI.create(url))
+               .build()) { // Mantén esto dentro del try-with-resources
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        if (response.statusCode() != 200) {
-            throw new RuntimeException("Failed to get response from the API: " + response.body());
-        }
-
-        Gson gson = new Gson();
-        JsonObject jsonResponse = gson.fromJson(response.body(), JsonObject.class);
-        JsonObject conversionRates = jsonResponse.getAsJsonObject("conversion_rates");
-
-        if (!conversionRates.has(toCurrency)) {
-            throw new RuntimeException("Invalid currency code: " + toCurrency);
-        }
-
-        double rate = conversionRates.get(toCurrency).getAsDouble();
-        return amount * rate;
-    }
-    private static String getCurrencyCodeFromAPI(String countryName) throws Exception {
-        String url = "https://restcountries.com/v3.1/name/" + countryName;
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        if (response.statusCode() != 200) {
-            throw new RuntimeException("Failed to get response from the API: " + response.body());
-        }
-        Gson gson = new Gson();
-        JsonElement jsonResponse = gson.fromJson(response.body(), JsonElement.class);
-        if (jsonResponse.isJsonArray()) {
-            JsonArray jsonArray = jsonResponse.getAsJsonArray();
-            if (jsonArray.size() == 0) {
-                throw new RuntimeException("No se encontró información para el país: " + countryName);
+            if (response.statusCode()!= 200) {
+                throw new CustomException("Failed to get response from the API: " + response.body());
             }
-            JsonObject firstResult = jsonArray.get(0).getAsJsonObject();
-            JsonObject currencyObj = firstResult.getAsJsonObject("currencies");
-            String currencyCode = currencyObj.keySet().iterator().next();
-            return currencyCode;
-        } else {
-            throw new RuntimeException("Respuesta inesperada de la API: " + jsonResponse);
+
+            Gson gson = new Gson();
+            JsonObject jsonResponse = gson.fromJson(response.body(), JsonObject.class);
+            JsonObject conversionRates = jsonResponse.getAsJsonObject("conversion_rates");
+
+            if (!conversionRates.has(toCurrency)) {
+                throw new CustomException("Invalid currency code: " + toCurrency);
+            }
+
+            double rate = conversionRates.get(toCurrency).getAsDouble();
+            return amount * rate;
+        } catch (Exception e) {
+            throw new CustomException("Error converting currency: " + e.getMessage());
+        } finally {
+            client.close(); // Cierra el HttpClient aquí
         }
     }
 
+
+    private static String getCurrencyCodeFromAPI(String countryCode) throws Exception {
+        String url = COUNTRIES_API_URL + "/name/" + countryCode;
+        HttpClient client = HttpClient.newHttpClient(); // Mueve esta línea fuera del try-with-resources
+
+        try (HttpRequest request = HttpRequest.newBuilder()
+               .uri(URI.create(url))
+               .build()) { // Mantén esto dentro del try-with-resources
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode()!= 200) {
+                throw new CustomException("Failed to get response from the API: " + response.body());
+            }
+
+            Gson gson = new Gson();
+            JsonElement jsonResponse = gson.fromJson(response.body(), JsonElement.class);
+
+            if (jsonResponse.isJsonArray()) {
+                JsonArray jsonArray = jsonResponse.getAsJsonArray();
+                if (jsonArray.size() == 0) {
+                    throw new CustomException("No se encontró información para el país: " + countryCode);
+                }
+                JsonObject firstResult = jsonArray.get(0).getAsJsonObject();
+                JsonObject currencyObj = firstResult.getAsJsonObject("currencies");
+                String currencyCode = currencyObj.keySet().iterator().next();
+                return currencyCode;
+            } else {
+                throw new CustomException("Respuesta inesperada de la API: " + jsonResponse);
+            }
+        } catch (Exception e) {
+            throw new CustomException("Error getting currency code: " + e.getMessage());
+        } finally {
+            client.close(); // Cierra el HttpClient aquí
+        }
+}
+
+
+    private enum ConversionOption {
+        EUR_TO_USD("EUR", "USD"),
+        EUR_TO_MXN("EUR", "MXN"),
+        USD_TO_EUR("USD", "EUR"),
+        USD_TO_MXN("USD", "MXN"),
+        MXN_TO_EUR("MXN", "EUR"),
+        MXN_TO_USD("MXN", "USD"),
+        CUSTOM("CUSTOM", "CUSTOM"),
+        EXIT("", "");
+
+        private final String fromCurrency;
+        private final String toCurrency;
+
+        ConversionOption(String fromCurrency, String toCurrency) {
+            this.fromCurrency = fromCurrency;
+            this.toCurrency = toCurrency;
+        }
+
+        public String getFromCurrency() {
+            return fromCurrency;
+        }
+
+        public String getToCurrency() {
+            return toCurrency;
+        }
+    }
+
+    static class CustomException extends Exception {
+        public CustomException(String message) {
+            super(message);
+        }
+    }
+
+    private static String readApiKey() {
+        Properties properties = new Properties();
+        try (InputStream input = new FileInputStream(CONFIG_FILE_PATH)) {
+            properties.load(input);
+            return properties.getProperty("api_key");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
 }
